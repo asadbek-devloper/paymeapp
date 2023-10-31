c94722e6d7a03e0888d5e9c49ba32bdfd9663513
AsadbekBoqiyev <boqiyevasadbek51@gmail.com>
2023-10-31 15:58:14 +0500

    tugadi

--- index.js
+++ index.js
@@ -9,6 +9,29 @@ const navbar = document.querySelector(".navbar");
 const mainNavbar = document.querySelector(".main-navbar");
 const exit = document.querySelector(".exit-button");
 const main = document.querySelector(".main");
+const history = document.querySelector(".history-card");
+const karta = document.querySelector(".karta-raqami");
+const summa = document.querySelector(".summa");
+const sending = document.querySelector(".sending");
+const incirement = document.querySelector(".incirement-value");
+const createElement = () => {
+  sending.addEventListener("click", (e) => {
+    e?.preventDefault();
+    history.insertAdjacentHTML(
+      "afterbegin",
+      `
+        <div class="historiy-card-itme">
+        <div class="historiy-text">
+          <h2>Boqiyev Asadbek</h2>
+          <p>19.01.2022 <span>18:50</span></p>
+        </div>
+        <h3>+${summa.value}$</h3>
+      </div>
+        
+        `
+    );
+  });
+};
 
 register.addEventListener("click", (e) => {
   e?.preventDefault();
@@ -21,7 +44,7 @@ register.addEventListener("click", (e) => {
       mainNavbar.classList.add("hidden");
       main.classList.remove("hidden");
     });
-    console.log(navbar);
+    createElement();
   } else {
     alert("Noto`g`ri malumot");
   }
