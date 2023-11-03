44d2a4d9d0c0746be8b2729d1bc253efbe57e8f7
AsadbekBoqiyev <boqiyevasadbek51@gmail.com>
2023-11-03 14:47:05 +0500

    login qismi tugadi

--- index.js
+++ index.js
@@ -1,65 +1,150 @@
 "use strict";
 
+let USERS = [
+  {
+    firstName: "Asadbek",
+    lastName: "Boqiyev ",
+    username: "boqiyev07",
+    password: "asadbek07",
+    card: {
+      cardTaype: "Visa",
+      balans: 16000,
+      fullName: "Asadbek Boqiyev",
+      cardNumber: 1234_5678_9756_1456,
+      expiredDate: "01 / 27",
+    },
+  },
+];
+
+let USER = null;
+
+let isVerify = false;
+
+//pages
+
+const loginPage = document.querySelector(".login-page");
+const paymentPage = document.querySelector(".payment-app");
+const alllHistory = document.querySelector(".all-history");
+
+// input
+
+const loginInput = document.querySelector(".login-input");
+const passwordInput = document.querySelector(".password-input");
+const transferCardNumInput = document.querySelector(".transfer-card-num");
+const transferAmountInput = document.querySelector(".transfer-amount");
+
+// buttons
+const loginButton = document.querySelector(".login-button");
+const logOutButton = document.querySelector(".log-out-btn");
+const transferBtn = document.querySelector(".transfer-btn");
 // elements
 
-const email = document.querySelector(".login");
-const register = document.querySelector(".register-button");
-const parol = document.querySelector(".parol");
-const navbar = document.querySelector(".navbar");
-const mainNavbar = document.querySelector(".main-navbar");
-const exit = document.querySelector(".exit-button");
-const main = document.querySelector(".main");
-const history = document.querySelector(".history-card");
-const karta = document.querySelector(".karta-raqami");
-const summa = document.querySelector(".summa");
-const sending = document.querySelector(".sending");
-const incirement = document.querySelector(".incirement-value");
-const user = document.querySelector(".user-name");
-const users = document.querySelector(".user");
-const cardnum = document.querySelector(".card-Num");
+const title = document.querySelector(".title");
+const cardTaype = document.querySelector(".card-type");
+const balans = document.querySelector(".amount");
+const cardFullName = document.querySelector(".fullName");
+const cardNumber = document.querySelector(".card-number");
+const cardExpiredDate = document.querySelector(".card-expired-date");
 
-// factions
+// custom
 
-const cardNum = () => {};
-
-const createElement = () => {
-  sending.addEventListener("click", (e) => {
-    e?.preventDefault();
-    history.insertAdjacentHTML(
-      "afterbegin",
-      `
-        <div class="historiy-card-itme">
-        <div class="historiy-text">
-          <h2>Boqiyev Asadbek</h2>
-          <p>19.01.2022 <span>18:50</span></p>
-        </div>
-        <h3>+${summa.value}$</h3>
-      </div>
-        
-        `
-    );
-    summa.value = "";
-  });
+const formatCardNumber = (cardNum) => {
+  cardNum = cardNum.toString();
+  const arr = [];
+  for (let i = 0; i < cardNum.length; i += 4) {
+    arr.push(cardNum.slice(i, i + 4));
+  }
+  return arr.join(" ");
 };
 
-register.addEventListener("click", (e) => {
-  e?.preventDefault();
-  user.insertAdjacentHTML("afterbegin", email.value);
-  users.insertAdjacentHTML("afterbegin", `${email.value}    `);
-  const userPasword = parol.value;
-  navbar.classList.add("hidden");
-  mainNavbar.classList.remove("hidden");
-  main.classList.remove("hidden");
-
-  exit?.addEventListener("click", (e) => {
-    mainNavbar.classList.add("hidden");
-    main.classList.add("hidden");
-    navbar.classList.remove("hidden");
+const writerOfCardInfo = () => {
+  cardTaype.textContent = USER.card.cardTaype;
+  balans.textContent = USER.card.balans.toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
   });
-  createElement();
-  switch (userPasword) {
-    case `asadbek07`:
-      cardnum.break;
+  cardFullName.textContent = USER.card.fullName;
+  cardNumber.textContent = formatCardNumber(USER.card.cardNumber);
+  cardExpiredDate.textContent = USER.card.expiredDate;
+};
+
+// event
+
+loginButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  const username = loginInput.value.trim();
+  const password = passwordInput.value.trim();
+  USERS.forEach((u) => {
+    if (u.username === username && u.password === password) {
+      USER = u;
+      isVerify = true;
+    }
+  });
+  if (!isVerify) {
+    alert("Xatolik bor!!!!!!!");
+    return;
   }
-  email.value = "";
+  loginInput.value = "";
+  passwordInput.value = "";
+
+  paymentPage.classList.remove("hidden");
+  loginPage.classList.add("hidden");
+  title.textContent = `Xush kelibsiz, ${USER.firstName} !`;
+  writerOfCardInfo();
+});
+
+transferBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("salom");
+  const trCardNum = +transferCardNumInput.value.trim();
+  const trAmount = +transferAmountInput.value.trim();
+
+  if (!trCardNum) {
+    alert("Xatolik a!!!");
+
+    return;
+  } else if (trCardNum.toString().length !== 16) {
+    alert("xatolik n!!!");
+    console.log(trCardNum);
+    return;
+  }
+  if (!trAmount || !(trAmount >= 1)) {
+    alert("Xatolik b!!!");
+    console.log(trAmount);
+    return;
+  } else if (USER.card.balans < trAmount) {
+    alert("Mablag`ingiz yetarli emas!!!");
+    return;
+  }
+  USER.card.balans = USER.card.balans - trAmount;
+  writerOfCardInfo();
+
+  alllHistory.insertAdjacentHTML(
+    "afterbegin",
+    `<li class="history-item">
+    <div class="fullName">
+      <p>asadbek</p>
+      <p>+6,600 $</p>
+    </div>
+    <span class="date">
+      <span>19.01.2022</span>
+      <span>18:50</span>
+    </span>
+  </li>`
+  );
+});
+
+logOutButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  paymentPage.classList.add("hidden");
+  loginPage.classList.remove("hidden");
+  cardTaype.textContent = " **** ";
+  balans.textContent = "****";
+  cardFullName.textContent = "*** ***";
+  cardNumber.textContent = "**** **** **** ****";
+  cardExpiredDate.textContent = "**/**";
+  title.textContent = "***";
+  USER = null;
 });
